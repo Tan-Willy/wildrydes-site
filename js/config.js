8245307f785a041d3cab9d23dc6c1756bf93115d
Willy <51873699+Tan-Willy@users.noreply.github.com>
2024-12-15 00:05:00 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_fngp4Mtx4', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6psbhq3a4bk4dtqk8thi0u976m', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
